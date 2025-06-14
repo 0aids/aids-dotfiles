eac8ce3d70d12bcc6f26ab44b887c5d4a9758bfc
0aids <aidanlldanu@gmail.com>
2025-06-14 22:11:01 +1200

    added some nvidia stuff for laptop

--- configs/nixos/config-global.nix
+++ configs/nixos/config-global.nix
@@ -5,6 +5,30 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    # Compare to the key published at https://nix-community.org/cache
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
+  nixpkgs.config.cudaSupport = true;
+  nixpkgs.config.allowUnfreePredicate = p:
+    builtins.all (
+      license:
+        license.free
+        || builtins.elem license.shortName [
+          "CUDA EULA"
+          "cuDNN EULA"
+          "cuTENSOR EULA"
+          "NVidia OptiX EULA"
+        ]
+    ) (
+      if builtins.isList p.meta.license
+      then p.meta.license
+      else [p.meta.license]
+    );
   documentation.enable = true;
   documentation.man.enable = true;
   documentation.man.generateCaches = false; #This makes rebuilding take forever.
